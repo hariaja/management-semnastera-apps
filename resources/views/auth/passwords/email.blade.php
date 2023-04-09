3d28cfed3b9b1e397d5efaf34e7bbb2837b2fad9
aldiamahari16 <aldiamahari16@gmail.com>
2023-04-09 13:14:13 +0700

    Done: Password Reset User

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,70 @@
-@extends('layouts.app')
-
+@extends('layouts.guest')
+@section('title', 'Lupa Kata Sandi')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+  <div class="hero-static d-flex align-items-center">
+    <div class="w-100">
+      <!-- Reminder Section -->
+      <div class="bg-body-extra-light">
+        <div class="content content-full">
+          <div class="row g-0 justify-content-center">
+            <div class="col-md-8 col-lg-6 col-xl-4 py-4 px-4 px-lg-5">
+              <!-- Header -->
+              <div class="text-center">
+                <p class="mb-2">
+                  <img class="img-avatar" src="{{ asset('assets/images/polikami.png') }}" alt="">
+                </p>
+                <h1 class="h4 mb-1">
+                  {{ trans('Lupa Kata Sandi') }}
+                </h1>
+                <p class="fw-medium text-muted mb-3">
+                  {{ trans('Harap berikan email atau nama pengguna akun Anda dan kami akan mengirimkan kata sandi Anda.') }}
+                </p>
+              </div>
+              <!-- END Header -->
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+              @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                  {{ session('status') }}
                 </div>
+              @endif
+
+              <!-- Reminder Form -->
+              <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="mb-4">
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror form-control-lg form-control-alt" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="{{ trans('Alamat Email') }}">
+                  @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+                </div>
+
+                <div class="row justify-content-center mb-4">
+                  <div class="col-lg-6 col-xxl-5">
+                    <button type="submit" class="btn w-100 btn-primary">
+                      <i class="fa fa-fw fa-envelope me-1"></i>
+                      {{ trans('Kirim Email') }}
+                    </button>
+                  </div>
+                </div>
+              </form>
+              <!-- END Reminder Form -->
+
+              <div class="text-center">
+                <a class="fs-sm fw-medium" href="{{ route('login') }}">{{ trans('Login?') }}</a>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
+      <!-- END Reminder Section -->
+
+      <!-- Footer -->
+      <div class="fs-sm text-center text-muted py-3">
+        <strong>{{ config('app.name') }}</strong> &copy; <span data-toggle="year-copy"></span>
+      </div>
+      <!-- END Footer -->
+
     </div>
-</div>
+  </div>
 @endsection
